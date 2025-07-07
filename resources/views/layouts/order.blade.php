cef1d687d30056cfc71f4b2891f7b0a589fc5c8e
Vlad <limitorg2016@ya.ru>
2025-07-07 23:37:34 +0300

    Добавлены og метатеги

--- resources/views/layouts/order.blade.php
+++ resources/views/layouts/order.blade.php
@@ -5,6 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
         <meta name="description" content="@yield('description')">
+        <meta property="og:title" content="@yield('title')" />
+        <meta property="og:description" content="@yield('description')" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="ru_RU" />
+        <meta property="og:site_name" content="МехПортал" />
+
         <link href="{{ asset('bootstrap/bootstrap-5.3.3-dist/css/bootstrap.min.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('bootstrap/bootstrap-icons-1.13.1/bootstrap-icons.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
@@ -13,6 +19,8 @@
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
         <link rel="manifest" href="/images/site.webmanifest">
         <meta name="application-name" content="МЕХПОРТАЛ - открытые заказы на металлообработку по всей России.">
+        <meta name="theme-color" content="#1c1c1c" />
+        <meta name="msapplication-navbutton-color" content="#1c1c1c" />
     </head>
 
     <body>
