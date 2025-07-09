cecc78798c0142858608babc0b14f77384c87d13
Vlad <limitorg2016@ya.ru>
2025-07-09 18:36:15 +0300

    Правки top-menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,8 @@
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
         <link rel="manifest" href="/images/site.webmanifest">
         <meta name="application-name" content="МЕХПОРТАЛ - открытые заказы на металлообработку по всей России.">
-        <meta name="theme-color" content="#1c1c1c" />
-        <meta name="msapplication-navbutton-color" content="#1c1c1c" />
+        <meta name="theme-color" content="#111" />
+        <meta name="msapplication-navbutton-color" content="#111" />
 
         <link rel="canonical" href="{{ url()->current() }}"/>
     </head>
