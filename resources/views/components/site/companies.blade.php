581f9839ea272336ca3fe8d68b6387e49da9019b
Vlad <limitorg2016@ya.ru>
2025-07-12 16:51:22 +0300

    sdfsdf

--- resources/views/components/site/companies.blade.php
+++ resources/views/components/site/companies.blade.php
@@ -5,73 +5,73 @@
     <div class="row gx-4 gy-4">
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/lukoil.jpg" class="img-fluid" alt="Заказы на металлообработку от компании Лукоил">
+                <img src="/images/company-logo/lukoil.webp" class="img-fluid" alt="Заказы на металлообработку от компании Лукоил">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/rosatom.jpg" class="img-fluid" alt="Заказы на металлообработку от компании Росатом">
+                <img src="/images/company-logo/rosatom.webp" class="img-fluid" alt="Заказы на металлообработку от компании Росатом">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/severstal.jpg" class="img-fluid" alt="Заказы на металлообработку от компании Северсталь">
+                <img src="/images/company-logo/severstal.webp" class="img-fluid" alt="Заказы на металлообработку от компании Северсталь">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/rzd.jpg" class="img-fluid" alt="Заказы на металлообработку от компании РЖД">
+                <img src="/images/company-logo/rzd.webp" class="img-fluid" alt="Заказы на металлообработку от компании РЖД">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/rosteh.jpg" class="img-fluid" alt="Заказы на металлообработку от компании Ростех">
+                <img src="/images/company-logo/rosteh.webp" class="img-fluid" alt="Заказы на металлообработку от компании Ростех">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/gazprom.jpg" class="img-fluid" alt="Заказы на металлообработку от компании Газпром Нефть">
+                <img src="/images/company-logo/gazprom.webp" class="img-fluid" alt="Заказы на металлообработку от компании Газпром Нефть">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/umg.jpg" class="img-fluid" alt="Заказы на металлообработку от компании UMG SDM">
+                <img src="/images/company-logo/umg.webp" class="img-fluid" alt="Заказы на металлообработку от компании UMG SDM">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/ural.jpg" class="img-fluid" alt="Заказы на металлообработку от компании URAL">
+                <img src="/images/company-logo/ural.webp" class="img-fluid" alt="Заказы на металлообработку от компании URAL">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/segezha.jpg" class="img-fluid" alt="Заказы на металлообработку от компании Segezha Group">
+                <img src="/images/company-logo/segezha.webp" class="img-fluid" alt="Заказы на металлообработку от компании Segezha Group">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/eurocement.jpg" class="img-fluid" alt="Заказы на металлообработку от компании Евроцемент">
+                <img src="/images/company-logo/eurocement.webp" class="img-fluid" alt="Заказы на металлообработку от компании Евроцемент">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/redwings.jpg" class="img-fluid" alt="Заказы на металлообработку от компании Redwings Airlines">
+                <img src="/images/company-logo/redwings.webp" class="img-fluid" alt="Заказы на металлообработку от компании Redwings Airlines">
             </div>
         </div>
 
         <div class="col-6 col-md-2">
             <div class="col-12 block-company">
-                <img src="/images/company-logo/ummc.jpg" class="img-fluid" alt="Заказы на металлообработку от компании УГМК">
+                <img src="/images/company-logo/ummc.webp" class="img-fluid" alt="Заказы на металлообработку от компании УГМК">
             </div>
         </div>
     </div>
