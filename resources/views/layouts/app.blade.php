0bcc2c25c7d58b9618a9336c5459964380a05507
Vlad <limitorg2016@ya.ru>
2025-07-12 14:23:28 +0300

    rel preload css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <meta property="og:locale" content="ru_RU" />
         <meta property="og:site_name" content="МехПортал" />
         
-        <link rel="preload" href="{{ asset('bootstrap/bootstrap-5.3.3-dist/css/bootstrap.min.css') }}" as="style">
+        <link rel="stylesheet" href="{{ asset('bootstrap/bootstrap-5.3.3-dist/css/bootstrap.min.css') }}" >
         <link rel="preload" href="{{ asset('bootstrap/bootstrap-icons-1.13.1/bootstrap-icons.min.css') }}" as="style">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
