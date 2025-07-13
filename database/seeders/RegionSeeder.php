13d9e872a162f8f9b04e432f9a36af7dfe05fc84
Vlad <limitorg2016@ya.ru>
2025-07-13 23:25:00 +0300

    fghfgh

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -13,16 +13,114 @@ class RegionSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::table('regions')->delete();
+
         DB::table('regions')->insert([
-            'name' => 'Санкт-Петербург',
-            'name_in' => 'в Санкт-Петербурге',
-            'slug' => 'spb',
+            'name' => 'Алтайский край',
+            'name_in' => 'в Алтайском крае',
+            'slug' => 'barnaul',
         ]);
 
         DB::table('regions')->insert([
-            'name' => 'Москва',
-            'name_in' => 'в Москве',
-            'slug' => 'msk',
+            'name' => 'Белгородская область',
+            'name_in' => 'в Белгородской области',
+            'slug' => 'belgorod',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Брянская область',
+            'name_in' => 'в Брянской области',
+            'slug' => 'bryansk',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Брянская область',
+            'name_in' => 'в Брянской области',
+            'slug' => 'bryansk',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Владимирская область',
+            'name_in' => 'во Владимирской области',
+            'slug' => 'vladimir',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Волгоградская область',
+            'name_in' => 'в Волгоградской области',
+            'slug' => 'volgograd',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Вологодская область',
+            'name_in' => 'в Вологодской области',
+            'slug' => 'vologda',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Воронежская область',
+            'name_in' => 'в Воронежской области',
+            'slug' => 'voronezh',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Донецкая Народная Республика',
+            'name_in' => 'в Донецкой Народной Республику',
+            'slug' => 'donetsk',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Запорожская область',
+            'name_in' => 'в Запорожской области',
+            'slug' => 'melitopol',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Ивановская область',
+            'name_in' => 'в Ивановской области',
+            'slug' => 'ivanovo',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Иркутская область',
+            'name_in' => 'в Иркутской области',
+            'slug' => 'irkutsk',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Кабардино-Балкарская Республика',
+            'name_in' => 'в Кабардино-Балкарской Республике',
+            'slug' => 'nalchik',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Калужская область',
+            'name_in' => 'в Калужской области',
+            'slug' => 'kaluga',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Кемеровская область',
+            'name_in' => 'в Кемеровской области',
+            'slug' => 'kemerovo',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Кировская область',
+            'name_in' => 'в Кировской области',
+            'slug' => 'kirov',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Костромская область',
+            'name_in' => 'в Костромской области',
+            'slug' => 'kostroma',
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Краснодарский край',
+            'name_in' => 'в Краснодарском крае',
+            'slug' => 'krasnodar',
         ]);
     }
 }
--- resources/views/components/modal-regions.blade.php
+++ resources/views/components/modal-regions.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal city-->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-regions">
     <div class="modal-content">
       <div class="modal-header"><b>Выберите регион</b>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -8,8 +8,8 @@
       <div class="modal-body">
           <div class="row"> 
             @foreach ($cities as $city) 
-            <div class="col-6 text-center mb-3">
-                <a href="/{{$city->slug}}">{{$city->name}}</a>
+            <div class="col-6 col-md-4 mb-2">
+                <a href="/{{$city->slug}}" class="region-link">{{$city->name}}</a>
             </div>
             @endforeach
           </div>
