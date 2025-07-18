c61e057e5f6b9262b7767747884ba70a9c622272
VladislavGorbunov <75179847+VladislavGorbunov@users.noreply.github.com>
2025-07-18 12:57:25 +0300

    add sitemap

--- app/Http/Controllers/Admin/SitemapController.php
+++ app/Http/Controllers/Admin/SitemapController.php
@@ -18,7 +18,8 @@ class SitemapController extends Controller
         $services = Service::get();
         $regions = Region::get();
 
-        $sitemap = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+        $sitemap = '<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
         
         // Статические страницы
         $sitemap .= '
