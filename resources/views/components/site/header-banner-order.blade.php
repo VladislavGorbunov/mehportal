7ad82e7768a11461c674147acb7a395c24d4c3de
Vlad <limitorg2016@ya.ru>
2025-07-24 19:03:44 +0300

    dgdfgd

--- resources/views/components/site/header-banner-order.blade.php
+++ resources/views/components/site/header-banner-order.blade.php
@@ -1,12 +1,12 @@
-<div class="container position-relative">
+<div class="position-relative">
     <div class="header-banner-order mt-0 mb-4 d-flex justify-content-center align-items-center flex-column">
-        <div class="col-12 col-md-8">
+    <div class="d-flex justify-content-center align-items-center container">   
+    <div class="col-12 col-md-8">
             <h1 class="text-center fs-4 p-0 m-0">{{ $headerTitle }}</h1>
-            
         </div>
         
         <div class="header-banner-order-layout"></div>
-    </div>
+    </div></div>
 
 </div>
 
