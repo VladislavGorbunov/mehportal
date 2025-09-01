3edf2629d534a6be55fecf1f7377a07992cce834
Vlad <limitorg2016@ya.ru>
2025-09-01 23:01:12 +0300

    Добавлен функционал отправки КП по заказам

--- resources/views/components/site/order-block.blade.php
+++ resources/views/components/site/order-block.blade.php
@@ -66,11 +66,11 @@
 
                     <x-site.order.cp-buttons :order="$order"/>
 
-                    <a href="/order/{{ $order['order_id'] }}" class="btn btn-more mb-2 me-3 d-flex align-items-center justify-content-center" target="_blank">Подробнее о заказе</a>
+                    <a href="/order/{{ $order['order_id'] }}" class="btn btn-more col-12 col-md-3 mb-2 me-3 d-flex align-items-center justify-content-center" target="_blank">Подробнее о заказе</a>
                     @if (!empty($order['order_archive']))
-                        <a href="{{ Storage::disk('orders_files')->url($order['order_archive']) }}" class="btn btn-file-download mb-2"><i class="bi bi-cloud-arrow-down"></i> Скачать файлы</a>
+                        <a href="{{ Storage::disk('orders_files')->url($order['order_archive']) }}" class="btn btn-file-download col-12 col-md-3 mb-2"><i class="bi bi-cloud-arrow-down"></i> Скачать файлы</a>
                     @else
-                        <a class="btn btn-file-download-noactive mb-2 d-flex align-items-center justify-content-center"><i class="bi bi-cloud-arrow-down"></i> Файлы отсутствуют</a>
+                        <a class="btn btn-file-download-noactive mb-2 col-12 col-md-3 d-flex align-items-center justify-content-center"><i class="bi bi-cloud-arrow-down"></i> Файлы отсутствуют</a>
                     @endif
                 </div>
             </div>
--- resources/views/components/site/order/cp-buttons.blade.php
+++ resources/views/components/site/order/cp-buttons.blade.php
@@ -1,14 +1,14 @@
 @if ($order['active'])
     @if (! Auth::guard('executor')->user())
-        <button type="button" class="btn btn-cp mb-2 me-3 d-flex align-items-center justify-content-center" data-bs-toggle="modal" data-bs-target="#modal-reg-cp"><i class="bi bi-lock"></i> Отправить КП</button>
+        <button type="button" class="btn btn-cp col-12 col-md-3 mb-2 me-3 d-flex align-items-center justify-content-center" data-bs-toggle="modal" data-bs-target="#modal-reg-cp"><i class="bi bi-lock"></i> Отправить КП</button>
     @else
         @if (! Auth::guard('executor')->user()->executorCompanies)
-            <button type="button" class="btn btn-cp mb-2 me-3 d-flex align-items-center justify-content-center" data-bs-toggle="modal" data-bs-target="#modal-add-company-cp"><i class="bi bi-lock"></i> Отправить КП</button>
+            <button type="button" class="btn btn-cp col-12 col-md-3 mb-2 me-3 d-flex align-items-center justify-content-center" data-bs-toggle="modal" data-bs-target="#modal-add-company-cp"><i class="bi bi-lock"></i> Отправить КП</button>
         @else
             @if (!Auth::guard('executor')->user()->premium && !$order['customer_premium'])
-                <button type="button" class="btn btn-cp mb-2 me-3 d-flex align-items-center justify-content-center" data-bs-toggle="modal" data-bs-target="#modal-reg-cp-no-premium"><i class="bi bi-file-earmark-text"></i> Отправить КП</button>
+                <button type="button" class="btn btn-cp col-12 col-md-3 mb-2 me-3 d-flex align-items-center justify-content-center" data-bs-toggle="modal" data-bs-target="#modal-reg-cp-no-premium"><i class="bi bi-file-earmark-text"></i> Отправить КП</button>
             @else
-                <a href="/order/{{ $order['order_id'] }}/send-cp" class="btn btn-cp mb-2 me-3 d-flex align-items-center justify-content-center" target="_blank"><i class="bi bi-file-earmark-text"></i> Отправить КП</a>
+                <a href="/order/{{ $order['order_id'] }}/send-cp" class="btn btn-cp col-12 col-md-3 mb-2 me-3 d-flex align-items-center justify-content-center" target="_blank"><i class="bi bi-file-earmark-text"></i> Отправить КП</a>
             @endif
         @endif
     @endif
